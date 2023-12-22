88b4aea7b2a7e83663cdfed360379477a56e4b90
unknown <gwenmouv26@gmail.com>
2023-12-22 18:05:18 +0100

    Abandonned the idea of deploying

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = (app) => {
     app.use(
         "/api/*",
         createProxyMiddleware({
-            target: "bvkdwalzcdu54em8zei7-mysql.services.clever-cloud.com",
+            target: "http://127.0.0.1:8000",
             secure: false,
         })
     );
