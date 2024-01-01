80cfc323a5960040d4852bfb1859d4d39acffdc3
unknown <gwenmouv26@gmail.com>
2024-01-01 14:51:46 +0100

    Small addition on legal notices page

--- src/pages/LegalNotices/LeagalNotices.js
+++ src/pages/LegalNotices/LeagalNotices.js
@@ -6,7 +6,7 @@ export default function LeagalNotices() {
         <main>
             <section className={`page-width center-horizontal ${style.legal_notices}`}>
                 <h1>Mentions légales</h1>
-                <p>Conformément aux dispositons des articles 6-III et 19 de la <Link to="https://www.legifrance.gouv.fr/loda/id/JORFTEXT000000801164">loi n°2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique</Link>, il est porté à la connaissance des utilisateurs du site "", les présentes mentions légales.</p>
+                <p>Conformément aux dispositons des articles 6-III et 19 de la <Link to="https://www.legifrance.gouv.fr/loda/id/JORFTEXT000000801164">loi n°2004-575 du 21 juin 2004 pour la confiance dans l'économie numérique</Link>, il est porté à la connaissance des utilisateurs du site "https://role-initiative.vercel.app", les présentes mentions légales.</p>
                 <article>
                     <h2>Édition, développement et intégration du site</h2>
                     <p className={`${style.addresses}`}>
